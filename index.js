46be3611796050644c1763b598370cec2ba34f38
Jannis R <mail@jannisr.de>
2018-05-17 00:35:12 +0200

    implementation :sparkles:

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
 'use strict'
 
-// todo
+const lineSlice = require('@turf/line-slice')
+
+const nonEmptyStr = str => 'string' === typeof str && str.length > 0
+const assertValidStation = (s, name) => {
+	if (!s) throw new Error('missing ' + name)
+	if (!nonEmptyStr(s.name)) throw new Error(`invalid ${name}.name`)
+	if (!s.location) throw new Error(`missing ${name}.location`)
+	if ('number' !== typeof s.location.latitude) {
+		throw new Error(`missing ${name}.location.latitude`)
+	}
+	if ('number' !== typeof s.location.longitude) {
+		throw new Error(`missing ${name}.location.longitude`)
+	}
+}
+
+// To find the "track slice" the vehicle has travelled and will travel, we
+// - query the whole journey (leg), including the track shape
+// - compute the slice of the track between previous station and next station
+// - check if `point` is close to the track slice
+const fetchTrackSlice = (hafas, prevStation, nextStation, journeyId, lineName) => {
+	assertValidStation(prevStation, 'prevStation')
+	assertValidStation(nextStation, 'nextStation')
+	if (!nonEmptyStr(journeyId)) {
+		throw new Error('journeyId must be a non-empty string')
+	}
+	if (!nonEmptyStr(lineName)) {
+		throw new Error('lineName must be a non-empty string')
+	}
+
+	return hafas.journeyLeg(journeyId, lineName, {
+		polyline: true,
+		passedStations: true
+	})
+	.then((leg) => {
+		const findPassed = (id) => {
+			return leg.passed.some(s => s.station && s.station.id === id)
+		}
+		if (!findPassed(prevStation.id)) {
+			throw new Error(prevStation.id + ' is not among the passed stations')
+		}
+		if (!findPassed(nextStation.id)) {
+			throw new Error(nextStation.id + ' is not among the passed stations')
+		}
+
+		const track = {
+			type: 'Feature',
+			properties: {},
+			geometry: {
+				type: 'LineString',
+				// todo: flatten leg.polyline to just `Point`s
+				coordinates: leg.polyline.features.map(f => f.geometry.coordinates)
+			}
+		}
+
+		const slice = lineSlice([
+			prevStation.location.longitude,
+			prevStation.location.latitude
+		], [
+			nextStation.location.longitude,
+			nextStation.location.latitude
+		], track)
+		return slice && slice.geometry
+	})
+}
+
+module.exports = fetchTrackSlice
